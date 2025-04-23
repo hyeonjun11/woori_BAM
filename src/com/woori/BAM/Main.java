c4c3457fae2d35283ba163940f774977b1957af2
hyeonjun11 <amazefizz@gmail.com>
2025-04-23 17:33:45 +0900

    시험문제

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -4,41 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
-		int i = 1;
-		
-		while (true) {
-			System.out.printf("명령어) ");
-			String cmd = sc.nextLine().trim();
-			
-			if (cmd.equals("exit")) {
-			break;
-			
-			}
-			if (cmd.length() == 0) {
-				System.out.println("명령어를 입력해주세요");
-				continue;
-			}
-			
-			
-			if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
-			 
-			} else if (cmd.equals("article write")) {
-				System.out.println("제목을 입력하시오");
-				System.out.println("제목: ");
-				String 제목 = sc.nextLine();
-				System.out.println("내용: " );
-				String 내용 = sc.nextLine();
-				System.out.println(i+"번째 게시글입니다");
-				i++;
-			} else  
-				System.out.println("존재하지 않는 명령어입니다.");
-				}
-
+		int i;
 	
-		System.out.println("== 프로그램 종료 ==");
-		sc.close();
+		System.out.println("학생의 수를 입력하세요.");
+		int num = sc.nextInt();
+		int[] scores = new int[num]; 
+		
+		System.out.println("학생의 이름을 입력하세요.");
+		String[] names = new String[num];
+		
+		
+		System.out.println("학생의 점수를 입력하세요.");
+		int[] score = new int[num];
+		
+		switch () {
+		case 10: 
+			System.out.println("A");
+			break;
+		case 9:
+			System.out.println("B");
+			break;
+		default:
+			System.out.println("C");
+		}
+		
+		
+		for(i=0; i<num;i++) {
+			System.out.println("학");
+		}
+		
+		
+		
+
 	}
 }
--- src/com/woori/BAM/Main2.java
+++ src/com/woori/BAM/Main2.java
@@ -1,46 +1,17 @@
 package com.woori.BAM;
+
 import java.util.Scanner;
+
 public class Main2 {
 	public static void main(String[] args) {
-		System.out.println("== 프로그램 시작 ==");
-		Scanner sc = new Scanner(System.in); 
-       
-		int id = 1;	 //while 문 안에 있으면 1이 반복되기때문에 while 문 밖에 선언
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		String name = sc.nextLine();
+		int score = sc.nextInt(); 
 		
-		while (true) {
-			System.out.printf("명령어) ");
-			String cmd = sc.nextLine().trim(); 
-			// trim은 입력받는 값의 공백을 제거
-			
-			
-			if (cmd.equals("exit")) {
-				break; // exit가 입력되면 while문을 빠져나간다.
-			} 
-			if (cmd.length() == 0) {  //cmd 문장의 길이가 0이면 실행 
-				System.out.println("명령어를 입력해 주세요");
-				continue;
-				// continue로 넘어가지 않으면 밑에 if문이 실행이 된다
-			}
-			
-			if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
-			} else if(cmd.equals("article write")){ // article list가 실행되지 않으면 실행 
-				
-				
-				System.out.printf("제목 : ");
-				String title = sc.nextLine().trim(); 
-				System.out.printf("내용 : ");
-				String body = sc.nextLine().trim(); 
-				System.out.println(id +"번글이 생성되었습니다");
-				id++;
-				
-			}else { 		// 위에 if 문이 실행되지않으면  실행 (위 if문이 실행되면 건너뛰고 while 문 처음부터 실행)
-				System.out.println("존재하지 않는 명령어 입니다");
-			}
-		}
-		
-		sc.close();
-		System.out.println("== 프로그램 종료 ==");
-	}
 
+		System.out.println( num);
+
+	}
 }
+
