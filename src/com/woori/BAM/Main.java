0ea7932c014895e7351566bfea681377b7dc093e
hyeonjun11 <amazefizz@gmail.com>
2025-04-23 12:44:24 +0900

    article write 구현

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -5,18 +5,39 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
-		Scanner sc = new Scanner(System.in); // 최적화 => 자원 1번 사용 개선
-		
-		while (true) {
-			System.out.printf("cmd)");
-			String cmd = sc.nextLine();// cmd 변수 ==> 재사용하기 위해서 
-			System.out.println("명령어 ) " + cmd);
-			if (cmd.equals("exit")) {
-				break;
-			}
+		Scanner sc = new Scanner(System.in);
 
-		}
-//		 qweqwewqeqwe
+		while (true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine().trim();
+			int i = 1;
+			
+			if (cmd.equals("exit")) {
+			break;
+			
+			}
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
+			
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			 
+			} else if (cmd.equals("article write")) {
+				System.out.println("제목을 입력하시오");
+				System.out.println("제목: ");
+				String 제목 = sc.nextLine();
+				System.out.println("내용: " );
+				String 내용 = sc.nextLine();
+				System.out.println(i+"번째 게시글입니다");
+				i++;
+			} else  
+				System.out.println("존재하지 않는 명령어입니다.");
+				}
+
+	
 		System.out.println("== 프로그램 종료 ==");
 		sc.close();
 	}
--- src/com/woori/BAM/Main2.java
+++ src/com/woori/BAM/Main2.java
@@ -1,15 +1,44 @@
 package com.woori.BAM;
 
+import java.util.Scanner;
+
 public class Main2 {
-public static void main(String[] args) {
-	String a = "asd";
-	String b = "asd";
-	System.out.println(a == b); 	// 주소가 같은가?
-	System.out.println(a.equals(b)); // a,b 주어진 값이 같은가?
-	
-	String c = new String("asd");
-	String d = new String("asd");
-	System.out.println(c == d); 	// 주소가 같은가?
-	System.out.println(c.equals(d)); // a,b 주어진 값이 같은가?
-}
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		Scanner sc = new Scanner(System.in); 
+		
+		while (true) {
+			
+			System.out.printf("명령어)");
+			String cmd = sc.nextLine().trim();
+			
+			
+			if (cmd.equals("exit")) {
+				break;
+			}
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해 주세요");
+				continue;
+			}
+			
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			} else if(cmd.equals("article write")){
+				
+				int id =1;
+				
+				System.out.printf("제목 :");
+				String title = sc.nextLine().trim();
+				System.out.printf("내용 :");
+				String body = sc.nextLine().trim();
+				System.out.println(id+ "번글이 생성되었습니다.");
+				id++;
+				
+			}else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+		}
+		}
+		System.out.println("== 프로그램 종료 ==");
+		sc.close();
+	}
 }
