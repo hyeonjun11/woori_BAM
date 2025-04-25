a0813d5be2d963e8ee31fd2eb69caf4638cda295
hyeonjun11 <amazefizz@gmail.com>
2025-04-25 17:31:36 +0900

    Article delete

--- src/com/woori/BAM/Main2.java
+++ src/com/woori/BAM/Main2.java
@@ -41,54 +41,79 @@ public class Main2 {
 				if (articles.size() == 0) {
 					System.out.println("존재하는 게시글이 없습니다");
 					continue;
-
 				}
 				System.out.printf("번호    |     제목\n");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 					System.out.printf("%d      |     %s\n", article.id, article.title);
-
-				}
-			} else if (cmd.startsWith("article detail ")) { // article detail 로 시작하니????
+				
+				
+			} 
+		
+			} else if (cmd.startsWith("article detail ")) { 
 				String[] cmdBits = cmd.split(" ");
 
-//				boolean articleChk = false;      // 객체지향프로그래밍 하기위한 준비
 				 
-				Article foundArticle = null;	// 객체로 대치(변수 vs 객체 차이점) 변수 1개 , 객체 (번호, 제목 ,내용)
+				Article foundArticle = null;	
 				
 				int id = 0;
-				try {   // Exception 발생 할 예상 코드 블럭 
-					id = Integer.parseInt(cmdBits[2]);  // 재정의 , 가독성(올라감) , 재활용(올라감)
+				try {   
+					id = Integer.parseInt(cmdBits[2]);  
 					
-				} catch (NumberFormatException e) { // (예외타입 변수명)
+				} catch (NumberFormatException e) { 
 					System.out.println("숫자를 입력하세요");
-					continue;   // 이하 실행이 안되도록 ==> while 작동 
+					continue;   
 				} catch (Exception e)	{
-					// 그 밖에 모든 Exception 처리한다
 				} 
-				
-				
-				
 				for (Article article : articles) {
-					if (article.id == id) { // 문자로 된 숫자를 ==> 숫자
+					if (article.id == id) { 
 						foundArticle = article;
 						break;
-					
 					}
 				}
-				
-//				if (articleChk == false) {
-				
-				if (foundArticle == null) {                       // 개발자 고민할 영역 ==> try catch 문 설명 
+				if (foundArticle == null) {                      
 					System.out.println(id + "번 게시물이 존재하지 않습니다"); 
-					continue;     	// 매우 매우 중요. 아래에서 NuLLPointException 발생이 안되도록 조치
+					continue;     	
 				}
-																			// 출력문을 밑으로 내린 이유 cmdBit[2] for문 중요
-				System.out.println("번호 :" + foundArticle.id); 				// 재활용을 위한 준비 ==> 많이 사용 ==> 메소드로 구성(메소드화)
+				System.out.println("번호 :" + foundArticle.id); 				
 				System.out.println("날짜 : ~~~ ");							
 				System.out.println("제목 :" + foundArticle.title);
 				System.out.println("내용 :" + foundArticle.body);
-			} else {
+				
+			} else if (cmd.startsWith("article delete ")) { 
+				String[] cmdBits = cmd.split(" ");
+
+				 
+				Article foundArticle = null;	
+				
+				int id = 0;
+				try {   
+					id = Integer.parseInt(cmdBits[2]);  
+					
+				} catch (NumberFormatException e) { 
+					System.out.println("숫자를 입력하세요");
+					continue;   
+				} catch (Exception e)	{
+				} 
+//	
+				for (Article article : articles) {
+					if (article.id == id) { 
+						foundArticle = article;
+						break;
+					}
+				}
+				if (foundArticle == null) {                      
+					System.out.println(id + "번 게시물이 존재하지 않습니다"); 
+					continue;     	
+				}
+				if (foundArticle != null) { 
+					articles.remove(id-1);
+					System.out.println(id + "번 게시물이 삭제되었습니다.");
+				}
+				
+				
+				
+				}else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
