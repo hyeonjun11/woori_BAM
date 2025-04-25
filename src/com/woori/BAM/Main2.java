a09d97ae447966c93c24ee87ea9bb8cd6431c9c9
hyeonjun11 <amazefizz@gmail.com>
2025-04-25 12:01:39 +0900

    최종본

--- src/com/woori/BAM/Main2.java
+++ src/com/woori/BAM/Main2.java
@@ -9,7 +9,7 @@ public class Main2 {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
 
-		int id = 1;
+		int lastArticleId = 1; // 게시글 번호 , 마지막게시글번호 수정
 		List<Article> articles = new ArrayList<>();
 
 		while (true) {
@@ -31,11 +31,11 @@ public class Main2 {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim();
 
-				Article article = new Article(id, title, body);
+				Article article = new Article(lastArticleId, title, body);
 				articles.add(article);
 
-				System.out.println(id + "번글이 생성되었습니다");
-				id++;
+				System.out.println(lastArticleId + "번글이 생성되었습니다");
+				lastArticleId++;
 
 			} else if (cmd.equals("article list")) {
 				if (articles.size() == 0) {
@@ -51,9 +51,29 @@ public class Main2 {
 				}
 			} else if (cmd.startsWith("article detail ")) { // article detail 로 시작하니????
 				String[] cmdBits = cmd.split(" ");
-				System.out.println(cmdBits[0]);
-				System.out.println(cmdBits[1]);
-				System.out.println(cmdBits[2]);
+
+//				boolean articleChk = false;      // 객체지향프로그래밍 하기위한 준비
+				Article foundArticle = null;	// 객체로 대치(변수 vs 객체 차이점) 변수 1개 , 객체 (번호, 제목 ,내용)
+				
+				int id = Integer.parseInt(cmdBits[2]);  // 재정의 , 가독성(올라감) , 재활용(올라감)
+
+				for (Article article : articles) {
+					if (article.id == id) { // 문자로 된 숫자를 ==> 숫자
+						foundArticle = article;
+						break;
+					}
+				}
+
+//				if (articleChk == false) {
+				if (foundArticle == null) {                       // 개발자 고민할 영역 ==> try catch 문 설명 
+					System.out.println(id + "번 게시물이 존재하지 않습니다"); 
+					continue;     	// 매우 매우 중요. 아래에서 NuLLPointException 발생이 안되도록 조치
+				}
+																			// 출력문을 밑으로 내린 이유 cmdBit[2] for문 중요
+				System.out.println("번호 :" + foundArticle.id); 				// 재활용을 위한 준비 ==> 많이 사용 ==> 메소드로 구성(메소드화)
+				System.out.println("날짜 : ~~~ ");							
+				System.out.println("제목 :" + foundArticle.title);
+				System.out.println("내용 :" + foundArticle.body);
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
